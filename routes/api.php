2b54d316f480cfc2f0444e3e6d2b35e0ef68da4c
C413bxD <caleb.895.9@gmail.com>
2023-05-29 22:39:39 -0500

    api estudiantes_apoderados

--- routes/api.php
+++ routes/api.php
@@ -40,11 +40,16 @@ Route::group(['prefix' => 'perfil'], function () {
 
 Route::get('v1/{dni}',function(Request $request, $dni){
     if($request->header('Authorization')=="cepreuna_v1_api")
-        return DB::select("SELECT concat(pe.inicio_ciclo,' - ',pe.fin_ciclo) AS periodo,pe.estado estado_periodo ,nro_documento, paterno, materno, nombres,celular ,case sexo when 1 then 'M' when 2 then 'F' END AS sexo ,ub.departamento,ub.provincia,ub.distrito ,
-        anio_egreso,co.denominacion AS name_cole, co.direccion AS dir_cole, co.departamento AS dep_cole,co.provincia AS pro_cole,co.distrito AS dis_cole,tc.denominacion AS tipo_cole  FROM estudiantes es
+        return DB::select("SELECT concat(pe.inicio_ciclo,' - ',pe.fin_ciclo) AS periodo,pe.estado estado_periodo ,es.nro_documento,es.nombres, es.paterno, es.materno,es.celular ,case sexo when 1 then 'M' when 2 then 'F' END AS sexo ,ub.departamento,ub.provincia,ub.distrito ,
+        anio_egreso,co.denominacion AS name_cole, co.direccion AS dir_cole, co.departamento AS dep_cole,co.provincia AS pro_cole,co.distrito AS dis_cole,tc.denominacion AS tipo_cole,
+        ap.nro_documento AS apo_documento,ap.nombres AS apo_nombres,ap.paterno AS apo_paterno,ap.materno AS apo_materno,ap.celular apo_celular,pa.denominacion as apo_parentesco  FROM estudiantes es
         INNER JOIN inscripciones ins ON ins.estudiantes_id=es.id
         INNER JOIN periodos pe ON pe.id=ins.periodos_id
         INNER JOIN ubigeos ub ON es.ubigeos_id = ub.id
         INNER JOIN colegios co ON co.id = es.colegios_id
-        INNER JOIN tipo_colegios tc ON tc.id = co.tipo_colegios_id where nro_documento=? and ins.correlativo<30",[$dni]);
+        INNER JOIN tipo_colegios tc ON tc.id = co.tipo_colegios_id
+        INNER JOIN estudiante_apoderados ea ON ea.estudiantes_id=es.id
+        INNER JOIN apoderados ap ON ap.id=ea.apoderados_id
+        INNER JOIN parentescos pa ON ap.parentescos_id=pa.id
+        where es.nro_documento=?",[$dni]);
 });
